94571571618e4060c8b87aaf788cf9f65481644d
Max Stubenbord <max.stubi@t-online.de>
2019-05-22 14:23:38 +0200

    Updates

---
+++ precache-manifest.c9e6f4976fce0263b0f24466c1485bf0.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f6bb320b20f282b10dddf78f5079fab",
+    "url": "/App-2-Semester/index.html"
+  },
+  {
+    "revision": "74248e81f2f2e13cbfc7",
+    "url": "/App-2-Semester/static/css/main.024487db.chunk.css"
+  },
+  {
+    "revision": "4d51854569ab381b4ecd",
+    "url": "/App-2-Semester/static/js/2.4c09892c.chunk.js"
+  },
+  {
+    "revision": "74248e81f2f2e13cbfc7",
+    "url": "/App-2-Semester/static/js/main.ade87092.chunk.js"
+  },
+  {
+    "revision": "772bcd09f6d181ea8d76",
+    "url": "/App-2-Semester/static/js/runtime~main.62a5e199.js"
+  },
+  {
+    "revision": "77f9465a6aa33e86ecd5bb6b4cca91f6",
+    "url": "/App-2-Semester/static/media/Rocket.77f9465a.png"
+  },
+  {
+    "revision": "b0b9fc0f16c66c33472009735005141f",
+    "url": "/App-2-Semester/static/media/clouds.b0b9fc0f.png"
+  },
+  {
+    "revision": "b2969c1cb89316d1814da8b44b219e7e",
+    "url": "/App-2-Semester/static/media/foggy.b2969c1c.png"
+  },
+  {
+    "revision": "de4648d062bcee24ccf45098d0be6ae4",
+    "url": "/App-2-Semester/static/media/rain.de4648d0.png"
+  },
+  {
+    "revision": "4518ab2eb9008ed8da569da0413678b9",
+    "url": "/App-2-Semester/static/media/snowy.4518ab2e.png"
+  },
+  {
+    "revision": "4b1e436815a2857a84f358be36905fdf",
+    "url": "/App-2-Semester/static/media/sunny.4b1e4368.png"
+  },
+  {
+    "revision": "b6fd85404d4e5df2db258889b5ad90ea",
+    "url": "/App-2-Semester/static/media/temperature-celsius.b6fd8540.svg"
+  },
+  {
+    "revision": "183e63fa4faeaafa94ac85f3418c7db4",
+    "url": "/App-2-Semester/static/media/thunder.183e63fa.png"
+  },
+  {
+    "revision": "5114ea503bb4cc3ba95295072ef1900e",
+    "url": "/App-2-Semester/static/media/weather-cloudy.5114ea50.svg"
+  },
+  {
+    "revision": "03386151d95868f471d9482317ca3164",
+    "url": "/App-2-Semester/static/media/weather-fog.03386151.svg"
+  },
+  {
+    "revision": "a3ca1232f547be1e883f048dba2f4325",
+    "url": "/App-2-Semester/static/media/weather-lightning.a3ca1232.svg"
+  },
+  {
+    "revision": "decb46736fe5dd6a1b60d052b78c80b3",
+    "url": "/App-2-Semester/static/media/weather-partlycloudy.decb4673.svg"
+  },
+  {
+    "revision": "8266bfd8ce52d7f393af2bf0a3d3ce67",
+    "url": "/App-2-Semester/static/media/weather-pouring.8266bfd8.svg"
+  },
+  {
+    "revision": "49de1d6e73a42053e252979be88fa122",
+    "url": "/App-2-Semester/static/media/weather-rainy.49de1d6e.svg"
+  },
+  {
+    "revision": "37dc85d13675c144c16ca3838eace11c",
+    "url": "/App-2-Semester/static/media/weather-snowy.37dc85d1.svg"
+  },
+  {
+    "revision": "ba3761342d0900071b222051ab558cdd",
+    "url": "/App-2-Semester/static/media/weather-sunny.ba376134.svg"
+  }
+]);
